775a87def0d7e05acd0b25ddc8bf1248d60197b9
Marcus <pionezero@protonmail.ch>
2024-04-22 23:19:06 -0500

    push

---
+++ port/sites/crazywaterrestaurant/js/toggle.js
@@ -0,0 +1,11 @@
+/* css [ thanks to: http://forums.devshed.com/css-help-116/platform-sniffer-and-css-17071.html ] */
+
+<!-- 
+var isMac = (navigator.appVersion.indexOf("Mac")!=-1) ? true : false; 
+if(isMac) { 
+    document.write('<link href="css/index_mac.css" rel="stylesheet" type="text/css">') 
+} 
+else { 
+    document.write('<link rel="stylesheet" href="css/index_mac.css" type="text/css">') 
+}
+// -->
